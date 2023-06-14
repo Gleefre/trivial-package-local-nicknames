b16c1208509665a2f00fd7257ad02e984ddda096
Gleefre <varedif.a.s@gmail.com>
2023-06-14 23:19:32 +0200

    Add bit of comments

--- tests-setup.lisp
+++ tests-setup.lisp
@@ -6,6 +6,7 @@
 (defun reset-test-packages ()
   (trivial-package-locks:without-package-locks
     (with-packages-cleanup (#!#:test #!#:test-1 #!#:test/2 #!#:test-2)))
+  ;; partly for old tests
   (defpackage #!#:test
     (:use)
     (:nicknames #!#:test/global-nick)
@@ -13,9 +14,12 @@
   (defpackage #!#:test/2
     (:use)
     (:export #:sym))
+
+  ;; old tests
   (defpackage #!#:test-1
     (:use)
-    (:local-nicknames (#:l #:cl) (#:nick #!#:test)))
+    (:local-nicknames (#:l #:cl)
+                      (#:nick #!#:test)))
   (defpackage #!#:test-2
     (:use)
     (:export "CONS")))
