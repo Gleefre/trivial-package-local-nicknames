b5ef70c07c318576965ec75c56c7fa9c7d5dd27d
Gleefre <varedif.a.s@gmail.com>
2023-06-14 20:24:16 +0200

    Add comments

--- tests/defpackage.lisp
+++ tests/defpackage.lisp
@@ -4,14 +4,19 @@
 (named-readtables:in-readtable trivial-package-lockal-nicknames.test)
 
 (define-test defpackage-one (#!#:a #!#:b #!#:c)
+  ;; uninterned symbol as nickname
   (defpackage #!#:a
     (:use)
     (:local-nicknames (#:nick #!#:test)))
   (assert-local-nicknames #!#:a (#:nick #!#:test))
+
+  ;; string as nickname
   (defpackage #!#:b
     (:use)
     (:local-nicknames ("NICK" #!#:test)))
   (assert-local-nicknames #!#:b (#:nick #!#:test))
+
+  ;; character as nickname
   (defpackage #!#:c
     (:use)
     (:local-nicknames (#\N #!#:test)))
