d0f630ce11303b2173d777d77cdda6efc89a3e13
Gleefre <varedif.a.s@gmail.com>
2023-06-14 20:31:57 +0200

    Add defpackage-same-nick test

--- tests/defpackage.lisp
+++ tests/defpackage.lisp
@@ -21,3 +21,33 @@
     (:use)
     (:local-nicknames (#\N #!#:test)))
   (assert-local-nicknames #!#:c (#:n #!#:test)))
+
+(define-test defpackage-same-nick (#!#:a #!#:b #!#:c #!#:d)
+  ;; same nickname for the same package is fine
+  ;; should not be repeated in (package-local-nicknames ...) alist
+  (defpackage #!#:a
+    (:use)
+    (:local-nicknames (#:nick #!#:test)
+                      (#:nick #!#:test)))
+  (assert-local-nicknames #!#:a (#:nick #!#:test))
+
+  ;; nicknames can be repeated in different options as well
+  (defpackage #!#:b
+    (:use)
+    (:local-nicknames (#:nick #!#:test))
+    (:local-nicknames (#:nick #!#:test)))
+  (assert-local-nicknames #!#:b (#:nick #!#:test))
+
+  ;; #:nick and "NICK" is a same nickname
+  (defpackage #!#:c
+    (:use)
+    (:local-nicknames (#:nick #!#:test)
+                      ("NICK" #!#:test)))
+  (assert-local-nicknames #!#:c (#:nick #!#:test))
+
+  ;; same nicknames for different packages are no go
+  (errors package-error
+    (defpackage #!#:d
+      (:use)
+      (:local-nicknames (#:nick #!#:test)
+                        (#:nick #!#:test/2)))))
